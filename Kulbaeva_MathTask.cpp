f38c97f25a843e61f3a13d84c3785efc90cb30de
sabinakulbaeva <sabinakulbaeva1@mail.ru>
2025-04-21 13:33:43 +0300

    commit2

--- Kulbaeva_MathTask.cpp
+++ Kulbaeva_MathTask.cpp
@@ -5,9 +5,10 @@ using namespace std;
 
 int main()
 {
-    int NumberA=0; NumberB =0;
+    int NumberA=0, NumberB =0;
     EnterDigit(NumberA, "input height A");
     EnterDigit(NumberB, "input height B");
     int RectangleArea = CalRectangleArea(NumberA, NumberB);
     cout <<"Area of Rectangle is" << RectangleArea <<endl;
+    
 }
