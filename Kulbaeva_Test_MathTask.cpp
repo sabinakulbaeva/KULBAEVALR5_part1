affa922766a87f8259d52575de8381c3a93871e3
sabinakulbaeva <sabinakulbaeva1@mail.ru>
2025-04-21 15:18:51 +0300

    Kulbaeva_Test_MathTask

--- Kulbaeva_Test_MathTask.cpp
+++ Kulbaeva_Test_MathTask.cpp
@@ -10,10 +10,39 @@ void testCalcRectangleArea(){
     int actual = CalcRectangleArea(a, b);
     ASSERT_EQUAL(expected, actual);
 }
+void testUserInput_Empty(){
+    string str="";
+    bool expected = false;
+    bool actual = UserInput(str);
+    ASSERT_EQUAL(expected, actual);
+}
+void testUserInput_Letter(){
+    string str= "a";
+    bool expected = false;
+    bool actual = UserInput(str);
+    ASSERT_EQUAL(expected,actual);
+}
+void testUserInput_DigitLetterValue(){
+    string str = "5a";
+    bool expected = false;
+    bool actual = UserInput(str);
+    ASSERT_EQUAL(expected,actual);
+}
+void testUserInput_NegativeValue(){
+    string str ="-5";
+    bool expected= false;
+    bool actual = UserInput(str);
+    ASSERT_EQUAL(expected,actual);
+}
 int main() {
     suite s;
     s.push_back(CUTE(testCalcRectangleArea));
+    s.push_back(CUTE(testUserInput_Empty));
+    s.push_back(CUTE(testUserInput_Letter));
+    s.push_back(CUTE(testUserInput_NegativeValue));
+    s.push_back(CUTE(testUserInput_DigitLetterValue));
+
     ide_listener<> listener;
-    makeRunner(listener)(s, "Test CalcRectangleArea");
+    makeRunner(listener)(s, "All Rectangle Tests");
     return 0;
 }
